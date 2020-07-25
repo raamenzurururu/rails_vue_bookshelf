11cfc0469e27eef23f501cb0c633dfb6ee763216
github raamenzurururu <reku9696@gmail.com>
2020-07-26 00:19:31 +0900

    final commit

---
+++ app/controllers/api/books_controller.rb
@@ -0,0 +1,11 @@
+class Api::BooksController < ApplicationController
+    def index
+        @books = Book.all
+        render 'index', formats: :json, handlers: 'jbuilder'
+    end 
+    
+    def show 
+        @book = Book.find(params[:id])
+        render 'show', formats: :json, handlers: 'jbuilder'
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+  resources :books, only: [:index]
+  namespace :api do
+    resources :books, only: [:index, :show]
+  end 
 end
